4eb3bb6045d4ab68a67907642c923094d96f9bb0
hashan789 <hashanmalind123@gmail.com>
2026-08-21 18:23:53 +0530

    created Homepage

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <div className='head-home'>
+            <h2 className='heading-2'>Create viral memes in seconds</h2>
+            <p className='p-2'>The most powerful meme generator with trending templates, and instant sharing</p>
+            <a href='/meme' className='btn-2'>Start Creating Free</a>
+        </div>
+    </div>
+  )
+}
